f34aacdad8f75629606a04f6673cf61f8a27feff
Dync_mark <chenpeng@dync.cc>
2019-05-24 14:09:50 +0800

    update ar-rtcp sdk, modify config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
     APP_ID: 'anyrtcy0UkK1PZydLX',
     APP_TOKEN: 'fabf75cfe0090ef495bf628cd7b9270b',
-    RTC_SERVER_URL: 'pro.anyrtc.io',//如果有部署私有云请配置私有云服务地址
+    // RTC_SERVER_URL: 'operation.anyrtc.cc',//如果有部署私有云请配置私有云服务地址
+    RTC_SERVER_URL: 'www.anyrtc.io',//如果有部署私有云请配置私有云服务地址
 };
