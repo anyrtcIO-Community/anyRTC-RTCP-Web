f2837cbdbd97640ce55eca353ad2b435dc94d5ea
Dync_mark <chenpeng@dync.cc>
2019-05-28 09:20:47 +0800

    update config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 export default {
-    APP_ID: 'anyrtcy0UkK1PZydLX',
-    APP_TOKEN: 'fabf75cfe0090ef495bf628cd7b9270b',
-    // RTC_SERVER_URL: 'operation.anyrtc.cc',//如果有部署私有云请配置私有云服务地址
-    RTC_SERVER_URL: 'www.anyrtc.io',//如果有部署私有云请配置私有云服务地址
+    APP_ID: '',
+    APP_TOKEN: '',
+    RTC_SERVER_URL: '',//如果有部署私有云请配置私有云服务地址
 };
